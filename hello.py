2f3a5a9895796b7d74d0e17f96a066da5d788e0e
aabid <aabidismail4@gmail.com>
2022-12-31 03:33:34 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+
+
+print(f"addition of 3 and 7 is {add(3,7)}")
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import add
+
+
+def test_add_1():
+    assert add(5, 6) == 11
+    assert add(2, 3) == 5
